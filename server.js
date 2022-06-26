5935793054bfe799506e967d6746219eb29bba0b
Oluwaseun007 <oluwaseun359@gmail.com>
2022-06-26 09:48:54 +0100

    server update

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-
+const testName = process.env.Name;
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    console.log(`${testName} rocks!`);
     await sleep(5000);
   }
 }
